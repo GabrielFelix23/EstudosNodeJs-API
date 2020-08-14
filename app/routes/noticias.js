2b1e420a52b61108980c518006cc592527289d40
GabrielFelix23 <srgfelix123@gmail.com>
2020-08-14 10:08:37 -0300

    Instalando Mysql.

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,4 +1,10 @@
 module.exports = (app) => {
+    const mysql = require("mysql")
+
+    const connection = mysql.createConnection({
+        
+    })
+
     app.get("/noticias", (req, res) => {
         res.render("noticias/noticias")
     })
