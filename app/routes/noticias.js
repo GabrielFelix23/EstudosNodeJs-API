c1029b7b8a9ceb02d3ee9f381e2b3b76dc628947
GabrielFelix23 <srgfelix123@gmail.com>
2020-08-17 11:56:50 -0300

    Configurando a chamada do bd.

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,10 +1,9 @@
-const dbConnection = require('../../config/dbConnecton')
-
 module.exports = (app) => {
-
-    const connection = dbConnection()
-
+    
     app.get("/noticias", (req, res) => {
+
+        const connection = app.config.dbConnecton()
+
         connection.query('select * from noticias', function(error,result) {
             res.render("noticias/noticias", {noticias:result})
         })
--- config/dbConnecton.js
+++ config/dbConnecton.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql")
 
-module.exports = function(app) {
+const connMySQL = function(){
+    console.log("Conexão com bd foi estabelecida")
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'gfsjxhs123',
         database: 'portal_noticias'
     })
+}
+
+module.exports = function(app) {
+    console.log("O autolocad carregou o módulo de conexão com o banco")
+    return connMySQL
 }
       
