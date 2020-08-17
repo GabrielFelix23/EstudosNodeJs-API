c9d12a97f4ab0b6049298e4fd25bde7c7d7b34a8
GabrielFelix23 <srgfelix123@gmail.com>
2020-08-17 11:28:17 -0300

    Configurando o consign.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const app = require('./config/server')
 
-const rotaHome = require("./app/routes/home")
+/*const rotaHome = require("./app/routes/home")
 rotaHome(app)
 
 const rotaFormInclusaoNoticia = require("./app/routes/formulario_inclusao_noticia")
 rotaFormInclusaoNoticia(app)
 
 const rotaNoticias = require("./app/routes/noticias")
-rotaNoticias(app)
+rotaNoticias(app)*/
 
 const porta = 3000
 app.listen(porta, () => {
--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
+const consign = require("consign")
+
 app.set('view engine', 'ejs')
 app.set('views', './app/views')
+
+consign().include('app/routes').into(app)
 
 module.exports = app
