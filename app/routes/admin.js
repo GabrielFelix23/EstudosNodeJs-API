bb332eee056e8eb1052fc4dca0c0b87808e9d064
GabrielFelix23 <srgfelix123@gmail.com>
2020-08-25 14:50:12 -0300

    Salvando no banco

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,17 @@
-module.exports = (app) => {
-    app.get("/formulario_inclusao_noticia", (req, res) => {
+module.exports = (aplication) => {
+    aplication.get("/formulario_inclusao_noticia", (req, res) => {
         res.render("admin/form_add_noticia")
     })
 
-    app.post("/noticias/salvar", (req, res) => {
-        var noticias = req.body;
-        res.send(noticias)
+    aplication.post("/noticias/salvar", (req, res) => {
+        var noticia = req.body;
+    
+        const connection = aplication.config.dbConnecton()
+        const noticiasModel = aplication.app.models.noticiasModel
+
+        noticiasModel.salvarNoticia(noticia, connection, function(error,result) {
+            res.redirect("/noticias")
+        })
     })
 }    
    
